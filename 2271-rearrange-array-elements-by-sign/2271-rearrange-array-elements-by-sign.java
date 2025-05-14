d060d66fea341b26beff617ed4bfedac3b250c2c
ajaykrishna9183 <83650651+ajaykrishna9183@users.noreply.github.com>
2025-05-13 23:56:51 -0400

    Time: 3 ms (100%), Space: 77.2 MB (48.86%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,32 +1,52 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int j=0,p=0,n=0;
-        List<Integer> p1 = new ArrayList<>();
-        List<Integer> n1 = new ArrayList<>();
-        int[] result = new int[nums.length];
+        int[] ans = new int[nums.length];
+        int pind = 0, negind = 1;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]>0)
-            {
-            p1.add(nums[i]);
-            }
             if(nums[i]<0)
             {
-            n1.add(nums[i]);
+                ans[negind] = nums[i]; 
+                negind = negind +2;
+            }
+            else{
+                ans[pind] = nums[i];
+                pind=pind+2;
+                
             }
         }
-        // while(j<nums.length){
-        // result[j++] = p1.get(p++);
-        // result[j++] = n1.get(n++);
-        // }
-        for(j=0;j<nums.length/2;j++)
-        {
-            result[2*j] = p1.get(p++);
-            result[2*j+1] = n1.get(n++);
-        }
+        return ans;
 
-    return result;
 
-        
+
+
+                
     }
 }
+
+// int j=0,p=0,n=0;
+//         List<Integer> p1 = new ArrayList<>();
+//         List<Integer> n1 = new ArrayList<>();
+//         int[] result = new int[nums.length];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]>0)
+//             {
+//             p1.add(nums[i]);
+//             }
+//             if(nums[i]<0)
+//             {
+//             n1.add(nums[i]);
+//             }
+//         }
+//         // while(j<nums.length){
+//         // result[j++] = p1.get(p++);
+//         // result[j++] = n1.get(n++);
+//         // }
+//         for(j=0;j<nums.length/2;j++)
+//         {
+//             result[2*j] = p1.get(p++);
+//             result[2*j+1] = n1.get(n++);
+//         }
+
+//     return result;
